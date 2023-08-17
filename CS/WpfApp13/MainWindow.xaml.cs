1083528eaacf9d3c3b3a06d03d07c019bf6f6b46
AndreySlabov <andrey.slabov@devexpress.com>
2023-08-17 14:54:18 +0400

    Update MainWindow.xaml.cs

--- CS/WpfApp13/MainWindow.xaml.cs
+++ CS/WpfApp13/MainWindow.xaml.cs
@@ -2,26 +2,9 @@
 using DevExpress.Utils.Serializing;
 using DevExpress.Xpf.Diagram;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp13
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+namespace WpfApp13 {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
@@ -40,9 +23,11 @@ namespace WpfApp13
 
         public void RegularFactoryItemTool(DiagramStencil stencil) {
             var itemTool = new FactoryItemTool("CustomShape1",
-               () => "Custom Shape 1",
-               diagram => new DiagramShape() { Content = "Predefined text" },
-               new System.Windows.Size(200, 200), false);
+                () => "Custom Shape 1",
+                diagram => new DiagramShape() { Content = "Predefined text" },
+                new System.Windows.Size(200, 200),
+                false
+            );
 
             stencil.RegisterTool(itemTool);
         }
@@ -55,9 +40,14 @@ namespace WpfApp13
             DiagramToolboxRegistrator.RegisterStencil(invisibleStencil);
 
             var itemTool = new FactoryItemTool("CustomShape2",
-               () => "Custom Shape 2",
-               diagram => new DiagramShapeEx() { Shape = DiagramToolboxRegistrator.GetStencil("InvisibleStencil").GetShape("Shape1"), CustomProperty = "Some value" },
-               new System.Windows.Size(200, 200), false);
+                () => "Custom Shape 2",
+                diagram => new DiagramShapeEx() { 
+                    Shape = DiagramToolboxRegistrator.GetStencil("InvisibleStencil").GetShape("Shape1"), 
+                    CustomProperty = "Some value" 
+                },
+                new System.Windows.Size(200, 200), 
+                false
+            );
 
             stencil.RegisterTool(itemTool);
         }
